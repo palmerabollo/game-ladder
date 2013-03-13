94f1ae49e0694c0cc2a1bf0da8503c94f0f8785c
labajo <jherlab@gmail.com>
2013-03-13 19:55:38 +0100

    Emojis :smile: :smile: :smile:

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,15 @@ String.prototype.shorten = function(max_length) {
     // TODO use max_length
     return this.replace(/^(.{15}[^\s]*).*/, "$1");
 }
+
+var emoji_set_up = function() {
+    emojify.setConfig({
+        emojify_tag_type: 'div',
+        emoticons_enabled: true,
+        people_enabled: true,
+        nature_enabled: true,
+        objects_enabled: true,
+        places_enabled: true,
+        symbols_enabled: true
+    });
+}
