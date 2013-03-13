f339147785b6c3d63167187b62313056fb5e2155
Alvaro Martínez Reol <amr@tid.es>
2013-03-13 18:25:33 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,3 @@ var plug_theme_switcher = function() {
 	  $('[tag="pageStyle"]').attr( "href", cssFile );
 	});
 };
-
-$('.comments input').keypress(function(event) {
-	if (event.keyCode == 13) {
-		$(this).parent().find('a.link.send').click();
-	}
-});
